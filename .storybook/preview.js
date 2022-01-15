77f8b69a83c1a82a4f2115cfc0c0f5b1c0a0f4dc
Matan Haliva <matanh@diagnosticrobotics.com>
2022-01-15 20:38:14 +0200

    change storybook config

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,50 @@
+import { createTheme } from "@mui/material/styles";
+import { INITIAL_VIEWPORTS } from "@storybook/addon-viewport";
+
+const theme = createTheme({
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 600,
+      md: 960,
+      lg: 1280,
+      xl: 1920,
+    },
+  },
+  shape: {
+    borderRadius: 8,
+  },
+  spacing: 8,
+});
+
+const customViewports = Object.entries(theme.breakpoints.values).reduce(
+  (viewports, [breakpoint, width]) => ({
+    ...viewports,
+    [breakpoint]: {
+      name: `${breakpoint} (min width)`,
+      styles: {
+        width: `${breakpoint === "xs" ? 320 : width}px`,
+        height: "100%",
+      },
+    },
+  }),
+  {}
+);
+
 export const parameters = {
+  viewport: {
+    viewports: {
+      ...customViewports,
+      ...INITIAL_VIEWPORTS,
+    },
+  },
+  grid: {
+    gridOn: false,
+    columns: 12,
+    gap: "16px",
+    gutter: "32px",
+    maxWidth: "600px",
+  },
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
     matchers: {
@@ -6,4 +52,4 @@ export const parameters = {
       date: /Date$/,
     },
   },
-}
+};
