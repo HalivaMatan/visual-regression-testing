c72787aea244b20ddd572b64f689cbcd51a7080c
Matan Haliva <matanh@diagnosticrobotics.com>
2022-01-15 19:50:58 +0200

    fix style

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,77 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Grid } from "@mui/material";
+import Header from "./components/Header";
+import Notifications from "./components/Notifications";
+import DashboardCard from "./components/DashboardCard";
+import Toggle from "./components/Toggle";
+import ProgressBar from "./components/ProgressBar";
+import LineChart from "./components/LineChart";
+import Parameters from "./components/Parameters";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Grid container>
+      <Grid item xs={12}>
+        <Header />
+      </Grid>
+
+      <Grid item container spacing={2} xs={12} padding={4}>
+        <Grid item md={8} xs={12}>
+          <DashboardCard title="Users" withHeader>
+            <LineChart />
+          </DashboardCard>
+        </Grid>
+
+        <Grid item md={4} xs={12}>
+          <DashboardCard title="Notifications" withHeader>
+            <Notifications />
+          </DashboardCard>
+        </Grid>
+
+        <Grid item md={3} xs={12} container spacing={2}>
+          <Parameters />
+        </Grid>
+
+        <Grid item md={6} xs={12}>
+          <DashboardCard title="Notifications" withHeader>
+            <Grid
+              marginTop={2}
+              height="100%"
+              container
+              spacing={2}
+              justifyContent="center"
+              alignItems="center"
+            >
+              <ProgressBar
+                title="Speed"
+                value={90}
+                maxValue={100}
+              ></ProgressBar>
+              <ProgressBar
+                title="Speed"
+                value={50}
+                maxValue={100}
+              ></ProgressBar>
+              <ProgressBar
+                title="Speed"
+                value={80}
+                maxValue={100}
+              ></ProgressBar>
+            </Grid>
+          </DashboardCard>
+        </Grid>
+        <Grid item md={3} xs={12}>
+          <DashboardCard>
+            <Toggle title="Lorem ipsum" />
+            <Toggle title="Lorem ipsum" />
+            <Toggle title="Lorem ipsum" />
+            <Toggle title="Lorem ipsum" />
+            <Toggle title="Lorem ipsum" />
+            <Toggle title="Lorem ipsum" />
+          </DashboardCard>
+        </Grid>
+      </Grid>
+    </Grid>
   );
 }
 
