492339d3ceb0972643b532ff3637043499fe0683
Matan Haliva <matanh@diagnosticrobotics.com>
2022-01-16 01:19:25 +0200

    change header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,7 +8,7 @@ const Header: FC<IHeaderProps> = () => {
     <AppBar position="static" style={{ backgroundColor: "#22242D" }}>
       <Toolbar>
         <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-          Dashboard
+          Dashboard Title
         </Typography>
         <Button color="inherit">Login</Button>
       </Toolbar>
