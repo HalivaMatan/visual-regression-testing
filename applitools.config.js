96753a36a65b3307183a0e40e4a95e0773048369
Matan Haliva <matanh@diagnosticrobotics.com>
2022-01-16 00:17:50 +0200

    add applitools

---
+++ applitools.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  browser: [
+    { width: 320, height: 1200, name: "chrome" },
+    { width: 600, height: 1200, name: "chrome" },
+    { width: 960, height: 1200, name: "chrome" },
+    { width: 1280, height: 1200, name: "chrome" },
+    { width: 1800, height: 1200, name: "chrome" },
+  ],
+};
